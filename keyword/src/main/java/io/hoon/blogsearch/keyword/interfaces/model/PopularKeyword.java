78bcbce7851e6d050562661eb50b94fd1f07ef6d
박일훈 <chiwoo2074@gmail.com>
2023-03-19 16:55:44 +0900

    검색어 모듈 개발
    1. db 의존성 제거
     - blog-search-api 이동 예정
    2. TestFixtures
    3. 도메인 모듈 기능 구현
    4. 단위 테스트 코드 작성

---
+++ keyword/src/main/java/io/hoon/blogsearch/keyword/interfaces/model/PopularKeyword.java
@@ -0,0 +1,39 @@
+package io.hoon.blogsearch.keyword.interfaces.model;
+
+import io.hoon.blogsearch.keyword.interfaces.exception.IllegalArgumentException;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.util.StringUtils;
+
+/**
+ * <h1>인기 검색어</h1>
+ */
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class PopularKeyword {
+
+    /**
+     * <h3>검색어</h3>
+     */
+    private final String name;
+
+    /**
+     * <h3>검색 횟수</h3>
+     */
+    private final long count;
+
+    public static PopularKeyword of(String name, long count) {
+        if (!StringUtils.hasText(name)) {
+            throw new IllegalArgumentException("name은 필수값 입니다.");
+        }
+        if (count < 0) {
+            throw new IllegalArgumentException("count는 0 미만일 수 없습니다.");
+        }
+        return new PopularKeyword(name, count);
+    }
+}
---
+++ keyword/src/main/java/io/hoon/blogsearch/keyword/repository/KeywordHistoryRepository.java
@@ -0,0 +1,25 @@
+package io.hoon.blogsearch.keyword.repository;
+
+import io.hoon.blogsearch.keyword.entity.KeywordHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface KeywordHistoryRepository extends JpaRepository<KeywordHistory, String> {
+
+    boolean existsByName(String name);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE KeywordHistory k set k.count = k.count + 1 where k.name = :name")
+    void increaseCount(@Param("name") String name);
+
+    Page<KeywordHistory> findAll(Pageable pageable);
+
+}
