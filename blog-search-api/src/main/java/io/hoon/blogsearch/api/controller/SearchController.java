d48d5af6ebb8867fc51da37d1e0e5856d16d6853
박일훈 <chiwoo2074@gmail.com>
2023-03-22 01:58:07 +0900

    인기 검색어 API 구현

--- blog-search-api/src/main/java/io/hoon/blogsearch/api/controller/SearchController.java
+++ blog-search-api/src/main/java/io/hoon/blogsearch/api/controller/SearchController.java
@@ -2,8 +2,11 @@ package io.hoon.blogsearch.api.controller;
 
 import io.hoon.blogsearch.api.event.publisher.BlogSearchEventPublisher;
 import io.hoon.blogsearch.api.manager.BlogSearchManager;
+import io.hoon.blogsearch.keyword.interfaces.model.PopularKeyword;
+import io.hoon.blogsearch.keyword.interfaces.service.KeywordService;
 import io.hoon.blogsearch.search.interfaces.enums.Sort;
 import io.hoon.blogsearch.search.interfaces.model.BlogSearchResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,18 +18,24 @@ import reactor.core.publisher.Mono;
  * <h1>블로그 검색 컨트롤러</h1>
  */
 @RestController
-@RequestMapping("/api/v1/search")
+@RequestMapping("/api/v1/blog")
 @RequiredArgsConstructor
 public class SearchController {
     private final BlogSearchManager blogSearchManager;
     private final BlogSearchEventPublisher blogSearchEventPublisher;
+    private final KeywordService keywordService;
 
-    @GetMapping
+    @GetMapping("/search")
     public Mono<BlogSearchResponse> search(@RequestParam String keyword,
                                            @RequestParam int page,
                                            @RequestParam(required = false, defaultValue = "10") int pageSize,
                                            @RequestParam(required = false, defaultValue = "ACCURACY") Sort sort) {
         blogSearchEventPublisher.publishBlogSearchEvent(keyword);
         return blogSearchManager.search(keyword, page, pageSize, sort);
+    }
+
+    @GetMapping("/popular")
+    public List<PopularKeyword> popularKeyword(@RequestParam(required = false, defaultValue = "10") int size) {
+        return keywordService.getPopularKeywords(size);
     }
 }
